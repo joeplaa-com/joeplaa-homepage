3b22ba722b0a561dd01dd882bc15571db5c3aef9
Joep van de Laarschot <joep@jodibooks.com>
2020-09-14 10:52:03 +0200

    Change navigation class

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -12,22 +12,28 @@ export default function Navigation({ className }: { className: string }) {
         return (path === href)
     }
 
+    function activeClass(href) {
+        const path = router.pathname;
+        const result = path === href ? 'linkNav-active' : 'linkNav';
+        return result
+    }
+
     return (
         <Nav className={className} navbar>
             <NavItem>
-                <NavLink href={navigation.home} active={activeLink(navigation.home)}>{data.Home}</NavLink>
+                <NavLink href={navigation.home} className={activeClass(navigation.home)} active={activeLink(navigation.home)}>{data.Home}</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink href={navigation.pricing} active={activeLink(navigation.pricing)}>{data.Pricing}</NavLink>
+                <NavLink href={navigation.pricing} className={activeClass(navigation.pricing)} active={activeLink(navigation.pricing)}>{data.Pricing}</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink href={navigation.portfolio} active={activeLink(navigation.portfolio)}>{data.Portfolio}</NavLink>
+                <NavLink href={navigation.portfolio} className={activeClass(navigation.portfolio)} active={activeLink(navigation.portfolio)}>{data.Portfolio}</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink href={navigation.howto} active={activeLink(navigation.howto)}>{data.Howto}</NavLink>
+                <NavLink href={navigation.howto} className={activeClass(navigation.howto)} active={activeLink(navigation.howto)}>{data.Howto}</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink href={navigation.blog} active={activeLink(navigation.blog)}>{data.Blog}</NavLink>
+                <NavLink href={navigation.blog} className={activeClass(navigation.blog)} active={activeLink(navigation.blog)}>{data.Blog}</NavLink>
             </NavItem>
         </Nav>
     );
--- src/lib/linkColor.ts
+++ src/lib/linkColor.ts
@@ -1,7 +1,5 @@
 export default function linkColor (colorString) {
-    if (colorString === 'dark') {
-        return 'linkDark';
-    } else if (colorString === 'light') {
+    if (colorString === 'light') {
         return 'linkLight';
     } else if (colorString === 'navbar') {
         return 'linkNav';
