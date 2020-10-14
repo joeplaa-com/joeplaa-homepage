5b0a1cdfd86937aeb51b0e99a34e81996fd4ccb6
Joep van de Laarschot <joep@jodibooks.com>
2020-10-14 16:54:58 +0200

    cleanup utils

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,3 @@
-export const supportedBrowsers = ['Chrome', 'Chromium', 'Edge', 'EdgeChromium', 'Firefox', 'Safari', 'Mobile Safari']
-export const unsupportedBrowsers = ['Internet Explorer', 'IE']
-export const urls = {
-    browserInfo: 'https://reactjs.org/docs/react-dom.html#browser-support',
-    chrome: 'https://www.google.com/chrome/',
-    edge: 'https://www.microsoft.com/nl-nl/edge/features',
-    firefox: 'https://www.mozilla.org/nl-NL/firefox/download/'
-}
 export const template = {
     copy: 'Website design by',
     license: 'MIT',
