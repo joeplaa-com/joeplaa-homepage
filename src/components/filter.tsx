423fc427fe0e546358a3b7c385b919ace7bfcae5
Joep van de Laarschot <joep@jodibooks.com>
2020-10-19 09:21:14 +0200

    filter back button alignment and icon

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { navigate } from '@reach/router'
 import { Button, Card, CardBody, Col, Row } from 'reactstrap'
-import { FaTimes } from 'react-icons/fa'
+import { IconContext } from 'react-icons'
+import { MdArrowBack } from 'react-icons/md'
 import Tag from '../components/tag'
 import { content } from '../utils/data'
 import { FilterProps } from '../types'
@@ -19,9 +20,9 @@ export default function Filter({ pathname, tags }: FilterProps) {
                         </Col>
                         {pathname.includes('tags')
                             ? <Col xs='auto' className='mt-2 mt-sm-0 ml-auto'>
-                                <Button outline color='primary' onClick={() => navigate(-1)} aria-label='filter-button'>
-                                    <span className={'d-none d-sm-inline'}>{' '}{content.Back}</span>
-                                    <FaTimes />
+                                <Button outline color='primary' onClick={() => navigate(-1)} className="d-inline-flex justify-content-center align-items-center" aria-label='filter-button'>
+                                    <IconContext.Provider value={{ size: '1.25rem' }}><MdArrowBack /></IconContext.Provider>
+                                    <span className={'d-none d-sm-flex mr-1'}>{content.Back}</span>
                                 </Button>
                             </Col>
                             : null}
