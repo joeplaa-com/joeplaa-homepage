f3e3a3526854eee702640d413a662520a22f0757
Joep van de Laarschot <joep@jodibooks.com>
2020-10-19 10:32:28 +0200

    styling tags

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Badge } from 'reactstrap'
 import { IconContext } from 'react-icons'
 import kebabCase from 'lodash/kebabCase'
 import TagIcon from './tagIcon'
@@ -8,10 +9,10 @@ import { TagProps } from '../types'
 const Tag = ({ tag }: TagProps) => {
     return (
         <Link to={`/tags/${kebabCase(tag.value)}`}>
-            <span className='tag badge badge-primary'>
+            <Badge color='primary' className='tag' href='#'>
                 <IconContext.Provider value={{ size: '1.5rem', className: 'mr-1' }}>{TagIcon(tag.value)}</IconContext.Provider>
                 <span>{tag.value.toUpperCase()}</span>
-            </span>
+            </Badge>
         </Link>
     );
 }
