1985dbbf6c01e3cbec0289a48a7126c2c1c97565
Joep van de Laarschot <joep@jodibooks.com>
2021-09-29 11:40:53 +0200

    use absolute positioning for copy button and add icon

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,6 +1,7 @@
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/nightOwl';
 import React, { ReactElement } from 'react';
+import { MdContentCopy } from 'react-icons/md';
 import { Button } from 'reactstrap';
 import { CodeProps } from '../types';
 import { content } from '../utils/content';
@@ -24,16 +25,18 @@ const Code = ({ codeString, language }: CodeProps): ReactElement => {
                 tokens,
                 getLineProps,
                 getTokenProps,
-            }): ReactElement => (<pre className={className} style={style}>
-                <Button outline size='sm' color='primary' className='float-right' onClick={handleClick}>{content.Copy}</Button>
-                {tokens.map((line, i) => (
-                    <div key={i} {...getLineProps({ line, key: i })}>
-                        {line.map((token, key) => (
-                            <span key={key} {...getTokenProps({ token, key })} />
-                        ))}
-                    </div>
-                ))}
-            </pre>)}
+            }): ReactElement => (<div style={{ position: 'relative' }}>
+                <pre className={className} style={{ ...style, position: 'relative' }}>
+                    {tokens.map((line, i) => (
+                        <div key={i} {...getLineProps({ line, key: i })}>
+                            {line.map((token, key) => (
+                                <span key={key} {...getTokenProps({ token, key })} />
+                            ))}
+                        </div>
+                    ))}
+                </pre>
+                <Button outline size='sm' color='primary' style={{ position: 'absolute', top: '8px', right: '8px' }} onClick={handleClick}><MdContentCopy />{content.Copy}</Button>
+            </div>)}
         </Highlight>
     );
 };
