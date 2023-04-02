104ffe4e75810251a4ab0278e5a807759e9c13ed
Joep van de Laarschot <joep@jodibooks.com>
2023-04-02 13:31:59 +0200

    initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,76 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        ecmaFeatures: {
-            jsx: true
-        },
-        sourceType: 'module'
-    },
     env: {
         browser: true,
-        node: true,
         es2020: true,
-        'jest/globals': true
-    },
-    settings: {
-        react: {
-            version: 'detect'
-        }
+        jest: true,
+        node: true
     },
     extends: [
         'standard',
+        'next',
         'eslint:recommended',
         'plugin:compat/recommended',
+        'plugin:jest-dom/recommended',
         'plugin:jsx-a11y/recommended',
-        'plugin:mdx/recommended',
         'plugin:optimize-regex/recommended',
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/eslint-recommended'
     ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        babelOptions: {
+            plugins: [
+                '@babel/plugin-proposal-class-properties'
+            ]
+        },
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2015, // try to use the oldest version possible to support as many old browsers as is reasonable, but don't overdo. 2015 = es6, which really is the minimum. Match this to "target" in tsconfig.json.
+        sourceType: 'module'
+    },
     plugins: [
-        '@typescript-eslint',
         'jest',
-        'promise'
+        'jest-dom',
+        'react',
+        '@typescript-eslint'
     ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
     rules: {
         indent: ['error', 4, { SwitchCase: 1 }],
         'linebreak-style': 0,
         quotes: ['error', 'single'],
         'no-console': 'warn',
+        'no-empty': 'warn',
         'no-unused-vars': 'off',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'warn', // Consider using explicit annotations for object literals and function return types even when they can be inferred.
+        '@typescript-eslint/no-empty-interface': [
+            'error',
+            {
+                allowSingleExtends: true
+            }
+        ],
         '@typescript-eslint/no-unused-vars': [
             'error',
             { vars: 'all', args: 'after-used', ignoreRestSiblings: false }
         ],
-        '@typescript-eslint/explicit-function-return-type': 'warn', // Consider using explicit annotations for object literals and function return types even when they can be inferred.
-        'no-empty': 'warn',
-        'no-return-assign': 'off',
-        'no-use-before-define': 'off',
         '@typescript-eslint/no-use-before-define': ['error'],
-        'prefer-regex-literals': 'off',
         'react/prop-types': 0,
         semi: [2, 'always'],
         'space-before-function-paren': ['warn', {
             anonymous: 'never',
             named: 'never',
             asyncArrow: 'always'
-        }]
+        }],
+        'testing-library/no-node-access': 'off', // TODO: [JODI-2053]
+        'testing-library/prefer-presence-queries': 'off', // TODO: [JODI-2053]
+        'tss-unused-classes/unused-classes': 'warn'
     }
 };
